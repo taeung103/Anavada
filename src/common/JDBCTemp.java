7a12782a9c5a5f1da8aa9dce8a8a0e8ef48d1939
tigermap <tigermap7@gmail.com>
2020-08-03 20:52:15 +0900

    oracle jdbc connection 세팅파일 생성 및 JDBC템플릿 클래스 생성 properties 파일은 추후 수정필요

---
+++ src/common/JDBCTemp.java
@@ -0,0 +1,82 @@
+package common;
+
+import java.io.*;
+import java.sql.*;
+import java.util.*;
+
+public class JDBCTemp {
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		Properties prop = new Properties();
+		
+		try {
+			String currentPath = JDBCTemp.class.getResource("./").getPath();
+			prop.load(new FileReader(currentPath + "jdbc.properties"));
+			
+			String driver = prop.getProperty("driver");
+			String url = prop.getProperty("url");
+			String user = prop.getProperty("user");
+			String passwd = prop.getProperty("passwd");
+						
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, user, passwd);
+			conn.setAutoCommit(false);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+
+	public static void close(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(Statement stmt) {
+		try {
+			if (stmt != null && !stmt.isClosed()) {
+				stmt.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void close(ResultSet rset) {
+		try {
+			if (rset != null && !rset.isClosed()) {
+				rset.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void commit(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.commit();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void rollback(Connection conn) {
+		try {
+			if (conn != null && !conn.isClosed()) {
+				conn.rollback();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
