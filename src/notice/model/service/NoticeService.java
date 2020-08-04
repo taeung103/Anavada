95b440a93c14fb8ddc73eb0070f6a9e54fc507e0
DESKTOP-QG3BN41\user1 <dydhslxl7@naver.com>
2020-08-04 17:10:09 +0900

    noticeService에 selectAll() 생성

--- src/notice/model/service/NoticeService.java
+++ src/notice/model/service/NoticeService.java
@@ -1,4 +1,20 @@
 package notice.model.service;
 
+import notice.model.dao.NoticeDao;
+import notice.model.vo.Notice;
+import java.util.ArrayList;
+import static common.JDBCTemp.*;
+import java.sql.Connection;
+
 public class NoticeService {
+	private NoticeDao ndao = new NoticeDao();
+	
+	public NoticeService() {}
+	
+	public ArrayList<Notice> selectAll() {
+		Connection conn = getConnection();
+		ArrayList<Notice> list = ndao.selectAll(conn);
+		close(conn);
+		return list;
+	}
 }
