79abf1ee9268c71cd13be8ad65352731886d15a0
DESKTOP-QG3BN41\user1 <tiger275458@gmail.com>
2020-08-12 14:18:29 +0900

    축제 게시판 댓글 model (dao, service, vo) 생성 + 추가

---
+++ src/fboardreply/model/dao/FboardReplyDao.java
@@ -0,0 +1,105 @@
+package fboardreply.model.dao;
+
+import static common.JDBCTemp.close;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import fboardreply.model.vo.FboardReply;
+import fboard.model.vo.Fboard;
+
+public class FboardReplyDao {
+	
+	public FboardReplyDao() {
+		
+	}
+
+	// 댓글 가지고오기
+	public ArrayList<FboardReply> selectList(Connection conn, int fboardNo) {
+		ArrayList<FboardReply> list = new ArrayList<FboardReply>();
+		PreparedStatement stmt = null;
+		ResultSet rset = null;
+
+		String query = "select * from fboard_reply where fboard_no = ? order by fboardreply_creatdate desc";
+
+		try {
+			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, fboardNo);
+			
+			rset = stmt.executeQuery();
+
+			while (rset.next()) {
+				FboardReply fboardR = new FboardReply();
+
+				fboardR.setFboardReplyNo(rset.getInt(1));
+				fboardR.setFboardNo(rset.getString(2));
+				fboardR.setMemberId(rset.getString(3));
+				fboardR.setFboardReplyContent(rset.getString(4));
+				fboardR.setFboardReplyLev(rset.getInt(5));
+				fboardR.setFboardReplyRef(rset.getInt(6));
+				fboardR.setFboardReplyCreatDate(rset.getString(7));
+
+				list.add(fboardR);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(stmt);
+		}
+
+		return list;
+	}
+
+	
+	//댓글 입력하기
+	public int insertFboardReply(Connection conn, FboardReply reply) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+
+		String query = "insert into fboard_reply values(SEQ_ID.NEXTVAL, ?, ?, ?, ?, ?, (select to_char(sysdate, 'YYYY.MM.DD HH:MI:SS') from dual))";
+
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, reply.getFboardNo());
+			pstmt.setString(2, reply.getMemberId());
+			pstmt.setString(3, reply.getFboardReplyContent());
+			pstmt.setInt(4, reply.getFboardReplyLev());
+			pstmt.setInt(5, reply.getFboardReplyRef());
+
+			result = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		return result;
+	}
+
+	//댓글 삭제하기
+	public int deleteFboardReply(Connection conn, int fboardReplyNo) {
+		int result = 0;
+		PreparedStatement pstmt = null;
+		
+		String query = "delete from fboard_reply where FBOARDREPLY_NO = ?";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, fboardReplyNo);
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+
+		return result;
+	}
+
+}
---
+++ src/fboardreply/model/vo/FboardReply.java
@@ -0,0 +1,123 @@
+package fboardreply.model.vo;
+
+public class FboardReply implements java.io.Serializable {
+
+	private static final long serialVersionUID = 5444L;
+	
+	private int fboardReplyNo;
+	private String fboardNo;
+	private String memberId;
+	private String fboardReplyContent;
+	private int fboardReplyLev;
+	private int fboardReplyRef;
+	private String fboardReplyCreatDate;
+	
+	public FboardReply() {
+	}
+	
+	
+	public FboardReply(int fboardReplyNo, String fboardNo, String memberId, String fboardReplyContent,
+			int fboardReplyLev, int fboardReplyRef, String fboardReplyCreatDate) {
+		super();
+		this.fboardReplyNo = fboardReplyNo;
+		this.fboardNo = fboardNo;
+		this.memberId = memberId;
+		this.fboardReplyContent = fboardReplyContent;
+		this.fboardReplyLev = fboardReplyLev;
+		this.fboardReplyRef = fboardReplyRef;
+		this.fboardReplyCreatDate = fboardReplyCreatDate;
+	}
+
+
+	public int getFboardReplyNo() {
+		return fboardReplyNo;
+	}
+
+
+	public void setFboardReplyNo(int fboardReplyNo) {
+		this.fboardReplyNo = fboardReplyNo;
+	}
+
+
+	public String getFboardNo() {
+		return fboardNo;
+	}
+
+
+	public void setFboardNo(String fboardNo) {
+		this.fboardNo = fboardNo;
+	}
+
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+
+	public String getFboardReplyContent() {
+		return fboardReplyContent;
+	}
+
+
+	public void setFboardReplyContent(String fboardReplyContent) {
+		this.fboardReplyContent = fboardReplyContent;
+	}
+
+
+	public int getFboardReplyLev() {
+		return fboardReplyLev;
+	}
+
+
+	public void setFboardReplyLev(int fboardReplyLev) {
+		this.fboardReplyLev = fboardReplyLev;
+	}
+
+
+	public int getFboardReplyRef() {
+		return fboardReplyRef;
+	}
+
+
+	public void setFboardReplyRef(int fboardReplyRef) {
+		this.fboardReplyRef = fboardReplyRef;
+	}
+
+
+	public String getFboardReplyCreatDate() {
+		return fboardReplyCreatDate;
+	}
+
+
+	public void setFboardReplyCreatDate(String fboardReplyCreatDate) {
+		this.fboardReplyCreatDate = fboardReplyCreatDate;
+	}
+
+
+	@Override
+	public String toString() {
+		return "FboardReply [fboardReplyNo=" + fboardReplyNo + ", fboardNo=" + fboardNo + ", memberId=" + memberId
+				+ ", fboardReplyContent=" + fboardReplyContent + ", fboardReplyLev=" + fboardReplyLev
+				+ ", fboardReplyRef=" + fboardReplyRef
+				+ ", fboardReplyCreatDate=" + fboardReplyCreatDate + "]";
+	}
+	
+	
+	
+
+	
+}
+
+//FBOARDREPLY_NO	NUMBER	No		1	댓글 번호
+//FBOARD_NO	VARCHAR2(30 BYTE)	No		2	게시판 번호
+//MEMBER_ID	VARCHAR2(20 BYTE)	No		3	작성자 id
+//FBOARDREPLY_CONTENT	VARCHAR2(4000 BYTE)	No		4	댓글 내용
+//FBOARDREPLY_LEV	NUMBER	No		5	글레벨
+//FBOARDREPLY_SEQ	NUMBER	No		6	댓글 순서
+//FBOARDREPLY_REF	NUMBER	No		7	댓글 번호
+//FBOARDREPLY_CREATDATE	DATE	No	"SYSDATE	"	8	댓글 작성날짜
